c72e593dcaf4a93d0bce001b8eb93e76de0a4b89
sllumpk1124 <153232657+sllumpk1124@users.noreply.github.com>
2024-02-14 12:09:55 -0500

    arrow_functions_practice

---
+++ app.js
@@ -0,0 +1,24 @@
+// refactor the following code:
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+const double = (arr) => arr.map((val) => val * 2);
+
+// replace all functions with arrow functions:
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareAndFindEvens = (numbers) => {
+const squares = numbers.map((num) => num ** 2);
+const evens = squares.filter((square) => square % 2 === 0);
+return evens;
+};
